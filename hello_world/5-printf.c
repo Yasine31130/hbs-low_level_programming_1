ddae1205c1a4309d928a48af1c3923ec0c90ca17
Yasine <yasine.bns@hotmail.com>
2023-06-16 15:39:44 -0700

    hello_world/

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+gcc -S -masm=intel $CFILE#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Allways 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
