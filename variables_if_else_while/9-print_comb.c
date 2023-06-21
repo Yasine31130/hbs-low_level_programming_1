bf8fd566dd1f648a04d455ede51c2d1a84de91d9
Yasine <yasine.bns@hotmail.com>
2023-06-21 05:04:43 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,17 +8,16 @@ int main(void)
 {
 	int i = 48;
 
-	while (i < 57)
+	while (i <= 57)
 	{
+		putchar(i);
 		if (i < 57)
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
 		i++;
 	}
-	putchar(i);
 	putchar('\n');
 	return (0);
 }
