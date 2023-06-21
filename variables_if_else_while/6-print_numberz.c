20c189bc676bb9878ff78dcaab615745bd08aacb
Yasine <yasine.bns@hotmail.com>
2023-06-21 01:20:19 -0700

    6. Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - DEscription
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
