f3f0ea3f3df52f26a2cf22d915bcaa2d576b16cf
Yasine <yasine.bns@hotmail.com>
2023-06-21 06:06:59 -0700

    1. I sometimes suffer from insomnia. And when I can t fall asleep, I play what I call the alphabet game

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - DEscription
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	return (0);
+}
+
