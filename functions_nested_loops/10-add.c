212b3b9c2c6a0d92fe1c4c756c49ab441b91ff53
Yasine <yasine.bns@hotmail.com>
2023-06-23 03:42:22 -0700

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Description
+ * @a: first integer
+ * @b: second integer
+ * Return: Add a + b
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
