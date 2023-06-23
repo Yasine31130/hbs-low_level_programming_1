209ac1d1ca550510a5c2cc9062e1523daed19c71
Yasine <yasine.bns@hotmail.com>
2023-06-23 08:46:09 -0700

    3. The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Description
+ * Return: if c is Uppercase shows 1 otherwise 0
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+}
