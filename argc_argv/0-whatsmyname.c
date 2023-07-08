79a65cdcee369f37c172ed77b59d181ed12ba531
Yasine <yasine.bns@hotmail.com>
2023-07-08 09:24:28 -0700

    C - argc, argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Description
+ * @argc: var
+ * @argv: var
+ * Return: name program
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
