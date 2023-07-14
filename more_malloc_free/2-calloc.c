62ef6678b525f6d3f41c5755fe2cc46432d4813e
Yasine <yasine.bns@hotmail.com>
2023-07-14 11:02:33 -0700

    2. _calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Description
+ * @nmemb: var
+ * @size: var
+ * Return:  function that allocates memory for an array, using malloc
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr = calloc(nmemb, size);
+
+	return (ptr);
+}
