0632dfcb3513cf1825f0133d690d2d483c1a977b
Yasine <yasine.bns@hotmail.com>
2023-07-20 08:07:10 -0700

    C - Function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *
+ *
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (*f == NULL)
+	{
+		f(NULL);
+	}
+	f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,3 @@
+#ifndef FUNCTION_POINTERS
+void print_name(char *name, void (*f)(char *));
+#endif
