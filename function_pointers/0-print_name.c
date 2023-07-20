d407a14917c8b01571c031a85d4deb7b7d6bca4c
Yasine <yasine.bns@hotmail.com>
2023-07-20 08:28:00 -0700

    0. What s my name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- *
- *
- *
+ * print_name - Description
+ * @name: pointer to char
+ * @f: pointe to function
+ * Return: nothing function that prints a name
  */
 void print_name(char *name, void (*f)(char *))
 {
